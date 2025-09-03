4b79af20204ecaeb07c4663ab5a2f537d112bba5
Ritik Kumar <dhacker401@gmail.com>
2025-09-04 00:09:01 +0530

    portfolio updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/",  // <-- yaha apna repo name daalna
+  base: "/First-Portfolio/",  // repo ka exact naam
 })
